945ba950e14b952976057d8fba359e73376cf682
Kyle KIM <k2wackie@gmail.com>
2022-05-11 11:57:24 +0900

    fix socket.io cors

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -31,7 +31,7 @@ const Chat = () => {
     });
     const data = chat ? (newChat ? chat.concat(newChat) : chat) : newChat;
     setChat(data);
-  }, [newChat, chat]);
+  }, [newChat, chat.length]);
 
   const handleChange = (e) => {
     const inputMSG = e.target.value;
