14a276dea735d2a2175367b07fa94d355ee7abe7
Kyle KIM <k2wackie@gmail.com>
2022-05-11 11:14:42 +0900

    fix socket.io cors

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const { ChatStorage } = require("../models/ChatStorage");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "https://git.heroku.com/ackie-pjt.git",
     methods: ["GET", "POST"],
   },
 });
